7ef1ac96b2c154f7dd6604192d6ffcd917c98630
Alexandre Lucas Oliveira Lima <xandelucas@gmail.com>
2024-05-28 21:54:08 -0400

    Ajuste validação

--- Commerce.Application/Services/CategoriaService.cs
+++ Commerce.Application/Services/CategoriaService.cs
@@ -5,57 +5,56 @@ using Commerce.Domain;
 using FluentValidation;
 
 
-namespace Commerce.Application.Services
+namespace Commerce.Application.Services;
+
+public class CategoriaService : ICategoriaService
 {
-    public class CategoriaService : ICategoriaService
+    private readonly ICategoriaRepository _categoriaRepository;
+    private readonly IMapper _mapper;
+    private static CategoriaDTOValidator _categoriaDTOValidator = new();
+
+    public CategoriaService(ICategoriaRepository categoriaRepository, IMapper mapper)
     {
-        private readonly ICategoriaRepository _categoriaRepository;
-        private readonly IMapper _mapper;
-        private static CategoriaDTOValidator _categoriaDTOValidator = new();
-
-        public CategoriaService(ICategoriaRepository categoriaRepository, IMapper mapper)
-        {
-            _categoriaRepository = categoriaRepository;
-            _mapper = mapper;
-        }
-        public async Task<Categoria> AtualizaCategoriaAsync(CategoriaDTO categoriaDTO)
-        {
-            _categoriaDTOValidator.ValidateAndThrow(categoriaDTO);
-            var categoria = _mapper.Map<Categoria>(categoriaDTO);
-            await _categoriaRepository.InsertAsync(categoria);
-            return categoria;
-        }
-
-        public async Task<Categoria> CriaCategoriaAsync(CategoriaDTO categoriaDTO)
-        {
-            _categoriaDTOValidator.ValidateAndThrow(categoriaDTO);
-            var categoria = _mapper.Map<Categoria>(categoriaDTO);
-            await _categoriaRepository.InsertAsync(categoria);
-            return categoria;
-        }
-
-        public async Task DeletaCategoriaAsync(long id)
-        {
-            await _categoriaRepository.DeleteAsync(id);
-        }
-
-        public async Task<ListaPaginada<CategoriaDTO>> GetAllCategoriasAsync(string nomeCampo, ConfiguracaoPaginacao configuracao, bool isAcendente = true)
-        {
-            return await _categoriaRepository.GetAllCategoriasAsync( nomeCampo,  configuracao, isAcendente );
-        }
-
-        public async Task<Categoria?> GetCategoriaByIdAsync(long id)
-        {
-            return await _categoriaRepository.GetByIdAsync(id);
-        }
-
+        this._categoriaRepository = categoriaRepository;
+        this._mapper = mapper;
     }
-    public class CategoriaDTOValidator : AbstractValidator<CategoriaDTO>
+    public async Task<Categoria> AtualizaCategoriaAsync(CategoriaDTO categoriaDTO)
     {
-        public CategoriaDTOValidator()
-        {
-            RuleFor(x => x.Nome).NotEmpty().MinimumLength(2).MaximumLength(64);
-            RuleFor(x => x.Descricao).NotEmpty().MinimumLength(3).MaximumLength(999);
-        }
+        _categoriaDTOValidator.ValidateAndThrow(categoriaDTO);
+        var categoria = _mapper.Map<Categoria>(categoriaDTO);
+        await _categoriaRepository.InsertAsync(categoria);
+        return categoria;
+    }
+
+    public async Task<Categoria> CriaCategoriaAsync(CategoriaDTO categoriaDTO)
+    {
+        _categoriaDTOValidator.ValidateAndThrow(categoriaDTO);
+        var categoria = _mapper.Map<Categoria>(categoriaDTO);
+        await _categoriaRepository.InsertAsync(categoria);
+        return categoria;
+    }
+
+    public async Task DeletaCategoriaAsync(long id)
+    {
+        await _categoriaRepository.DeleteAsync(id);
+    }
+
+    public async Task<ListaPaginada<CategoriaDTO>> GetAllCategoriasAsync(string nomeCampo, ConfiguracaoPaginacao configuracao, bool isAcendente = true)
+    {
+        return await _categoriaRepository.GetAllCategoriasAsync( nomeCampo,  configuracao, isAcendente );
+    }
+
+    public async Task<Categoria?> GetCategoriaByIdAsync(long id)
+    {
+        return await _categoriaRepository.GetByIdAsync(id);
+    }
+
+}
+public class CategoriaDTOValidator : AbstractValidator<CategoriaDTO>
+{
+    public CategoriaDTOValidator()
+    {
+        RuleFor(x => x.Nome).NotEmpty().MinimumLength(2).MaximumLength(64);
+        RuleFor(x => x.Descricao).NotEmpty().MinimumLength(3).MaximumLength(999);
     }
 }
